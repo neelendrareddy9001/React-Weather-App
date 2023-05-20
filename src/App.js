83e041cbef494816dd5182d52002c684a4ab7128
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-05-20 19:37:59 +0200

    Added

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+import axios from 'axios';
+
+function App() {
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=dallas&units=imperial&appid=0f4f8ebc9f0688ce0e0f21ee234dc374`
+
+
+  return (
+    <h2>Hello World</h2>
+  );
+}
+
+export default App;
