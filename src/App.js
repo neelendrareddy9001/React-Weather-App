99ce414b301a06307a0be98bbe47021d19c8a0f4
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-05-20 21:05:04 +0200

    Added

--- src/App.js
+++ src/App.js
@@ -35,23 +35,23 @@ function App() {
             <p>{data.name}</p>
           </div>
           <div className='temp'>
-            {data.main ? <h1>{data.main.temp}</h1> : null}
+            {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
           </div>
           <div className='description'>
-            <p>Clouds</p>
+            {data.weather ? <p>{data.weather[0].main}</p> : null}
           </div>
         </div>
         <div className='bottom'>
           <div className='feels'>
-            <p className='bold'>65°F</p>
+            {data.main ? <p className='bold'>{data.main.feels_like}°F</p> : null}
             <p>Feels Like</p>
           </div>
           <div className='himidity'>
-            <p className='bold'>20%</p>
+            {data.main ? <p className='bold'>{data.main.humidity}</p> : null}
             <p>Humidity</p>
           </div>
           <div className='humidity'>
-            <p className='bold'>12 MPh</p>
+            {data.main ? <p className='bold'>{data.wind.speed}</p> : null}
             <p>Wind Speed</p>
           </div>
         </div>
